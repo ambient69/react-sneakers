226d0ebab821ee37bb9785ec9b5cbfa7fc6503fc
ambient69 <137407062+ambient69@users.noreply.github.com>
2023-12-12 13:47:18 +0200

    Props & CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
+
+const arr = [
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    imageUrl: '/img/sneakers/1.jpg',
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 15699,
+    imageUrl: '/img/sneakers/2.jpg',
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 8499,
+    imageUrl: '/img/sneakers/3.jpg',
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 8999,
+    imageUrl: '/img/sneakers/4.jpg',
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
-      <Drawer />
+      {/* <Drawer /> */}
 
       <Header />
       <div className="content p-40">
@@ -15,10 +39,11 @@ function App() {
             <input type="text" placeholder="Поиск..." />
           </div>
         </div>
+
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import styles from './Card.module.scss';
 
-const Card = () => {
+const Card = (props) => {
+  const onClickButton = () => {
+    return alert('123');
+  };
+
   return (
-    <div className="card">
+    <div className={styles.card}>
       <div className="favorite">
         <img src="img/heart-unliked.svg" alt="unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
-        <button className="button">
+        <button onClick={onClickButton} className="button">
           <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
       </div>
